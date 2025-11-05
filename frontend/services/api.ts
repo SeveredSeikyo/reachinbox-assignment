dda14981f1e1cb4751056000bfc9b486011dbbc7
severedseikyo <thanmaysadguru5912@gmail.com>
2025-11-05 15:40:26 +0530

    added docker files

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,6 +1,6 @@
 import { EmailDocument, Account } from '../types';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = process.env.API_URL;
 
 export interface PaginatedResponse {
   emails: EmailDocument[];
